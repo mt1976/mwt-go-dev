c149cbe1bd64f5dbcaaf7637af689c8c73bfa8a7
Matt <mt76@gmx.com>
2021-04-21 21:03:33 +0100

    Refactoring Complete
    New Tools & Jobs packages created.
    
    Jobs is for  fxrefresh st

--- application/appSupport.go
+++ application/appSupport.go
@@ -302,29 +302,3 @@ func FormatCurrencyDps(inAmount string, inCCY string, inPrec string) string {
 	return ac.FormatMoney(bum)
 }
 
-//TruncateString truncates a string to a define number of characters
-func TruncateString(str string, length int) string {
-	if length <= 0 {
-		return ""
-	}
-
-	// This code cannot support Japanese
-	// orgLen := len(str)
-	// if orgLen <= length {
-	//     return str
-	// }
-	// return str[:length]
-
-	// Support Japanese
-	// Ref: Range loops https://blog.golang.org/strings
-	truncated := ""
-	count := 0
-	for _, char := range str {
-		truncated += string(char)
-		count++
-		if count >= length {
-			break
-		}
-	}
-	return truncated
-}
--- go.mod
+++ go.mod
@@ -3,11 +3,11 @@ module github.com/mt1976/mwt-go-dev
 go 1.15
 
 require (
+	github.com/davecgh/go-spew v1.1.1
 	github.com/denisenkom/go-mssqldb v0.9.0
-	github.com/go-delve/delve v1.6.0 // indirect
 	github.com/google/uuid v1.2.0
 	github.com/jimlawless/cfg v0.0.0-20160326141742-136e0c264d31
 	github.com/leekchan/accounting v1.0.0
 	github.com/mbndr/figlet4go v0.0.0-20190224160619-d6cef5b186ea
-	github.com/pkg/profile v0.0.0-20170413231811-06b906832ed0 // indirect
+	github.com/robfig/cron/v3 v3.0.1
 )
