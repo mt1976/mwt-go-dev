1a06df96153c9795a98463dee1f2908fe3dc6e61
Matt <mt76@gmx.com>
2021-07-22 12:49:03 +0100

    Upgrade to Bootstrap 5

--- siena/rateImporterModel.go
+++ siena/rateImporterModel.go
@@ -19,16 +19,25 @@ type RateImporterMsg struct {
 }
 
 type RateImporterMsgItem struct {
-	Text   string `xml:",chardata"`
-	Cat    string `xml:"cat,attr"`
-	ID     string `xml:"id,attr"`
-	Src    string `xml:"src,attr"`
-	Bid    string `xml:"bid"`
-	Ask    string `xml:"ask"`
-	Own    string `xml:"own"`
-	Rsk    string `xml:"rsk"`
-	DTme   string `xml:"dTme"`
-	Amt    string `xml:"amt"`
+	Text string `xml:",chardata"`
+	Cat  string `xml:"cat,attr"`
+	ID   string `xml:"id,attr"`
+	Src  string `xml:"src,attr"`
+	Uid  string `xml:"uid,attr"`
+	Bid  string `xml:"bid"`
+	Ask  string `xml:"ask"`
+	Own  string `xml:"own"`
+	Rsk  string `xml:"rsk"`
+	DTme string `xml:"dTme"`
+	Amt  string `xml:"amt"`
+	Vols struct {
+		Text string `xml:",chardata"`
+		Px   struct {
+			Text string `xml:",chardata"`
+			Stk  string `xml:"stk"`
+			Vol  string `xml:"vol"`
+		} `xml:"px"`
+	} `xml:"vols"`
 	AmtCcy string `xml:"amtCcy"`
 	Sts    string `xml:"sts"`
 }
@@ -64,6 +73,8 @@ func RateImporterTest() {
 	item.Rsk = "LON"
 	item.DTme = "2012-11-28T10:10:10"
 	item.Amt = "10000"
+	item.Vols.Px.Stk = "123.45"
+	item.Vols.Px.Vol = "456.78"
 	item.AmtCcy = "USD"
 	item.Sts = "OK"
 
